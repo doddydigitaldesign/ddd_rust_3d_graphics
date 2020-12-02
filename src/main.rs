3b51eea8ab250681ceef6983477078c5b2a83d20
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-12-02 21:09:33 +0100

    Rotate the cube on three axis

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,54 @@
-// #![feature(min_const_generics)]
-mod utils;
+#![feature(min_const_generics)]
+
 extern crate ddd_rust_3d_graphics;
+extern crate piston_window;
+extern crate vecmath;
 
 use ddd_rust_3d_graphics::{
-    create_window, get_projection, into_polygons::IntoPolygons, traits::structifyable::*,
-    traits::vectorizable::*, vector::Vec4, Mesh, Triangle,
+    create_window, get_projection, traits::structifyable::*, Mesh, Point, Triangle,
 };
 
-extern crate piston_window;
+use math::{rotate_radians, transform_pos};
 use piston_window::*;
-extern crate vecmath;
-// use types::Triangle;
-use utils::get_cube_colors;
-use vecmath::col_mat4_transform;
+use utils::{get_cube_colors, get_transformed_mesh, project_mesh, transform_cube};
+
+mod utils;
 
 fn main() {
-    // Create a cube mesh using triangles
-    const NUMBER_OF_TRIANGLES: usize = 12;
-    let triangles = utils::get_cube_triangles(0f64, 0f64, 0f64);
-    let colors = get_cube_colors();
-    let cube_mesh: Mesh<[Triangle; NUMBER_OF_TRIANGLES]> = Mesh::new(triangles);
-
     const SCREEN_HEIGHT: u64 = 1080u64;
     const SCREEN_WIDTH: u64 = 1920u64;
-    const F_NEAR_PLANE: f64 = 1f64;
-    const F_FAR_PLANE: f64 = 100f64;
-    const F_FIELD_OF_VIEW: f64 = 70f64;
+    const F_NEAR_PLANE: f64 = 0.01f64;
+    const F_FAR_PLANE: f64 = 1000f64;
+    const F_FIELD_OF_VIEW: f64 = 120f64;
     const F_ASPECT_RATIO: f64 = (SCREEN_WIDTH as f64) / (SCREEN_HEIGHT as f64);
+    let mut window = create_window("DDD Rust 3D Graphics", (SCREEN_WIDTH, SCREEN_HEIGHT));
 
     let projection = get_projection(F_ASPECT_RATIO, F_FIELD_OF_VIEW, F_FAR_PLANE, F_NEAR_PLANE);
-
-    let mut projected_triangles = [[[0f64; 4]; 4]; NUMBER_OF_TRIANGLES];
-    for (i, mesh_triangle) in cube_mesh.items.iter().enumerate() {
-        let mut projected_triangle: Vec4<Vec4<f64>> = [[0f64; 4]; 4];
-        for (j, triangle_point) in mesh_triangle.to_vector().iter().enumerate() {
-            let vec = [
-                triangle_point[0],
-                triangle_point[1],
-                triangle_point[2],
-                1f64,
-            ];
-            projected_triangle[j] = col_mat4_transform(projection, vec);
-        }
-        projected_triangles[i] = projected_triangle;
-    }
-
-    // Open a window and draw stuff
-
-    let mut window = create_window("DDD Rust 3D Graphics", (SCREEN_WIDTH, SCREEN_HEIGHT));
-    let projected_mesh: Mesh<[Triangle; NUMBER_OF_TRIANGLES]> =
-        Mesh::from_vector(&projected_triangles);
-    let mut polygons = [[[0f64; 2]; 3]; NUMBER_OF_TRIANGLES];
-
-    for (i, elem) in projected_mesh.items.iter().enumerate() {
-        let item = elem.into_polygon();
-
-        for (j, point) in item.iter().enumerate() {
-            polygons[i][j] =
-                math::transform_pos(math::translate([1920f64 / 2f64, 1080f64 / 2f64]), *point);
-        }
-    }
+    // Create a cube mesh using triangles
+    const NUMBER_OF_TRIANGLES: usize = 12;
+    let triangles = utils::get_cube_triangles(0f64, 0f64, 1000f64);
+    let colors = get_cube_colors();
+    let mut cube_mesh: Mesh<[Triangle; NUMBER_OF_TRIANGLES]> = Mesh::new(triangles);
 
     // Draw
     while let Some(e) = window.next() {
-        window.draw_2d(&e, |_c, g, _d| {
-            clear([1.0, 1.0, 1.0, 1.0], g);
-            for (i, polygon_triangle) in polygons.iter().enumerate() {
-                polygon(colors[i], polygon_triangle, _c.transform, g);
+        match e {
+            Event::Input(_, _) => {}
+            Event::Loop(_) => {
+                let transformed_cube = transform_cube(&mut cube_mesh);
+                let projected_triangles = project_mesh(&transformed_cube, &projection);
+                let projected_mesh: Mesh<[Triangle; NUMBER_OF_TRIANGLES]> =
+                    Mesh::from_vector(&projected_triangles);
+                let polygons = get_transformed_mesh(&projected_mesh);
+                window.draw_2d(&e, |_c, g, _d| {
+                    clear([1.0, 1.0, 1.0, 1.0], g);
+
+                    for (i, polygon_triangle) in polygons.iter().enumerate() {
+                        polygon(colors[i], polygon_triangle, _c.transform, g);
+                    }
+                });
             }
-        });
+            Event::Custom(_, _, _) => {}
+        }
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,19 +1,25 @@
-use ddd_rust_3d_graphics::{Point, Triangle};
-use piston_window::types::Color;
+use ddd_rust_3d_graphics::{
+    into_polygons::IntoPolygons, vector::Vec4, vectorizable::Vectorizable, Mesh, Point, Triangle,
+};
+use piston_window::{
+    math::{self, rotate_radians, transform_pos},
+    types::Color,
+};
+use vecmath::col_mat4_transform;
 
 pub fn get_cube_colors() -> [Color; 12] {
     [
         [1.0, 0.0, 0.0, 0.5],
-        [0.0, 1.0, 0.0, 0.5],
-        [0.0, 0.0, 1.0, 0.5],
+        [0.0, 1.0, 1.0, 0.5],
+        [0.5, 0.2, 1.0, 0.5],
         [1.0, 0.0, 0.0, 0.5],
+        [0.5, 1.0, 0.1, 0.5],
+        [0.0, 0.5, 1.0, 0.5],
+        [1.0, 0.0, 0.1, 0.5],
         [0.0, 1.0, 0.0, 0.5],
-        [0.0, 0.0, 1.0, 0.5],
+        [0.2, 0.0, 1.0, 0.5],
         [1.0, 0.0, 0.0, 0.5],
-        [0.0, 1.0, 0.0, 0.5],
-        [0.0, 0.0, 1.0, 0.5],
-        [1.0, 0.0, 0.0, 0.5],
-        [0.0, 1.0, 0.0, 0.5],
+        [0.3, 1.0, 0.0, 0.5],
         [0.0, 0.0, 1.0, 0.5],
     ]
 }
@@ -87,4 +93,64 @@ pub fn get_cube_triangles(origin_x: f64, origin_y: f64, origin_z: f64) -> [Trian
             Point::new(origin_x + 1f64, origin_y, origin_z),
         ),
     ]
+}
+
+pub fn project_mesh<const N: usize>(
+    mesh: &Mesh<[Triangle; N]>,
+    projection: &[[f64; 4]; 4],
+) -> [[[f64; 4]; 4]; N] {
+    let mut projected_items = [[[0f64; 4]; 4]; N];
+    for (i, mesh_triangle) in mesh.items.iter().enumerate() {
+        let mut projected_item: Vec4<Vec4<f64>> = [[0f64; 4]; 4];
+        for (j, triangle_point) in mesh_triangle.to_vector().iter().enumerate() {
+            let vec = [
+                triangle_point[0],
+                triangle_point[1],
+                triangle_point[2],
+                1f64,
+            ];
+            projected_item[j] = col_mat4_transform(*projection, vec);
+        }
+        projected_items[i] = projected_item;
+    }
+    projected_items
+}
+
+pub fn get_transformed_mesh<const N: usize>(mesh: &Mesh<[Triangle; N]>) -> [[[f64; 2]; 3]; N] {
+    let mut transformed_items = [[[0f64; 2]; 3]; N];
+    for (i, elem) in mesh.items.iter().enumerate() {
+        let item = elem.into_polygon();
+
+        for (j, point) in item.iter().enumerate() {
+            transformed_items[i][j] =
+                math::transform_pos(math::translate([1920f64 / 2f64, 1080f64 / 2f64]), *point);
+        }
+    }
+    transformed_items
+}
+
+pub fn transform_cube<const N: usize>(mesh: &mut Mesh<[Triangle; N]>) -> Mesh<[Triangle; N]> {
+    let mut new_triangles: [Triangle; N] = [mesh.items[0]; N];
+    for (i, elem) in mesh.items.iter_mut().enumerate() {
+        let rotation_matrix_a = rotate_radians(0.01f64 / 180f64);
+        let rotation_matrix_b = rotate_radians(0.01f64 / 180f64);
+        let rotation_matrix_c = rotate_radians(0.01f64 / 180f64);
+
+        let (a, b, c) = (elem.a, elem.b, elem.c);
+        let a_vec = transform_pos(rotation_matrix_a, a.to_vector2());
+        let b_vec = transform_pos(rotation_matrix_b, b.to_vector2());
+        let c_vec = transform_pos(rotation_matrix_c, c.to_vector2());
+
+        let triangle = Triangle::new(
+            Point::new(a_vec[0], a_vec[1], a.z),
+            Point::new(b_vec[0], b_vec[1], b.z),
+            Point::new(c_vec[0], c_vec[1], c.z),
+        );
+        elem.a = triangle.a;
+        elem.b = triangle.b;
+        elem.c = triangle.c;
+
+        new_triangles[i] = triangle;
+    }
+    Mesh::new(new_triangles)
 }
