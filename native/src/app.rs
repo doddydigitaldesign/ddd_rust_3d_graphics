b51a86b236f87d5d29d3922ca1f3615028a880bd
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-12-06 18:08:42 +0100

    Add some text, fine-tune drag

--- native/src/app.rs
+++ native/src/app.rs
@@ -1,7 +1,8 @@
+use graphics::{CharacterCache, Graphics};
 use opengl_graphics::GlGraphics;
 use piston::{Key, RenderArgs, UpdateArgs};
 
-use crate::{ACCELERATION_FACTOR, FPS_FACTOR, SCREEN_HEIGHT, SCREEN_WIDTH};
+use crate::{ACCELERATION_FACTOR, BALL_RADIUS, FPS_FACTOR, RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH};
 
 pub struct App {
     pub(crate) gl: GlGraphics, // OpenGL drawing backend.
@@ -14,57 +15,162 @@ pub struct App {
 }
 
 impl App {
-    pub(crate) fn render(&mut self, args: &RenderArgs) {
+    pub(crate) fn render<G: Graphics, C>(&mut self, args: &RenderArgs, glyphs: &mut C)
+    where
+        C: CharacterCache<Texture = opengl_graphics::Texture>,
+    {
         use graphics::*;
 
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        const BALL_COLOR: [f32; 4] = [0.0, 0.0, 0.55, 0.3];
 
         let radius = self.radius;
         let color = self.color;
-        let square = rectangle::square(0.0, 0.0, radius * 2.0);
+        let square = graphics::rectangle::square(0.0, 0.0, radius * 2.0);
+        let ball = graphics::ellipse::circle(0.0, 0.0, BALL_RADIUS);
         let rotation = self.rotation;
         let (_x_screen_mid, _y_screen_mid) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
         let [x, y] = self.position;
+        let speed = (self.velocity[0].powi(2) + self.velocity[1].powi(2)).sqrt();
+
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(BLACK, gl);
 
-            println!("Square position: {} x {}", x, y);
+            // println!("Square position: {} x {}", x, y);
 
-            let transform = c
+            let square_transform = c
                 .transform
                 .trans(x, y)
                 .rot_rad(rotation)
                 .trans(-radius, -radius);
+            let ball_transform = c.transform.trans(_x_screen_mid, _y_screen_mid);
+            let text_title_transform = c.transform.trans(100.0, 100.0);
+            let text_ball_transform = c.transform.trans(
+                args.window_size[0] / 2.0 - BALL_RADIUS / 2.0,
+                args.window_size[1] / 2.0,
+            );
+            let text_keys_transform = c.transform.trans(100.0, 150.0);
+            let text_speed_transform = c.transform.trans(100.0, 200.0);
+            let text_position_transform = c.transform.trans(100.0, 250.0);
 
-            // Draw a box rotating around the middle of the screen.
-            rectangle(color, square, transform, gl);
+            let text_ball = format!("Drag applied here!");
+            let text_keys =
+                format!("Keybindings: Acceleration - WASD, Break - Spacebar, ESC - Quit");
+            let text_position = format!("Position x: {} , y: {}", x.round(), y.round());
+            let text_speed = format!("Speed: {}", speed.round());
+
+            rectangle(color, square, square_transform, gl);
+            ellipse(BALL_COLOR, ball, ball_transform, gl);
+
+            graphics::text::Text::new_color(WHITE, 32)
+                .draw("Demo game", glyphs, &c.draw_state, text_title_transform, gl)
+                .ok();
+            graphics::text::Text::new_color(WHITE, 24)
+                .draw(
+                    text_ball.as_str(),
+                    glyphs,
+                    &c.draw_state,
+                    text_ball_transform,
+                    gl,
+                )
+                .ok();
+            graphics::text::Text::new_color(WHITE, 24)
+                .draw(
+                    text_keys.as_str(),
+                    glyphs,
+                    &c.draw_state,
+                    text_keys_transform,
+                    gl,
+                )
+                .ok();
+            graphics::text::Text::new_color(WHITE, 24)
+                .draw(
+                    text_position.as_str(),
+                    glyphs,
+                    &c.draw_state,
+                    text_position_transform,
+                    gl,
+                )
+                .ok();
+            graphics::text::Text::new_color(WHITE, 24)
+                .draw(
+                    text_speed.as_str(),
+                    glyphs,
+                    &c.draw_state,
+                    text_speed_transform,
+                    gl,
+                )
+                .ok();
         });
     }
 
     pub(crate) fn update(&mut self, args: &UpdateArgs) {
-        let [v_x, v_y] = self.velocity;
         let [x, y] = self.position;
+        let inside_ball = is_inside_ball([x, y]);
+
+        let [v_x, v_y] = self.velocity;
+        let speed = (v_x.powi(2) + v_y.powi(2)).sqrt();
+
+        // Update scale
+        self.radius = RADIUS + (if speed >= 50.0 { 50.0 } else { speed });
+        let area = 2.0 * self.radius;
 
         // Detect collision with walls
         let border_left = self.radius;
         let border_right = SCREEN_WIDTH - self.radius;
         let border_bottom = SCREEN_HEIGHT - self.radius;
         let border_top = self.radius;
+        fn is_inside_ball(pos: [f64; 2]) -> bool {
+            let vector_to_center = [980.0 - pos[0], 540.0 - pos[1]];
+            let distance_from_center =
+                ((vector_to_center[0]).powi(2) + (vector_to_center[1]).powi(2)).sqrt();
+
+            distance_from_center <= BALL_RADIUS
+        }
+        let drag_x = 0.5 * (1.0 / v_x.abs()) * v_x.powi(2) * area * args.dt;
+        let drag_y = 0.5 * (1.0 / v_y.abs()) * v_y.powi(2) * area * args.dt;
 
         let new_v_x = if x <= border_left || x >= border_right {
             -v_x * args.dt * FPS_FACTOR
         } else {
-            v_x * args.dt * FPS_FACTOR
+            let mut value = v_x;
+            if inside_ball {
+                if value > 1.0 {
+                    value = value - drag_x;
+                } else if value < -1.0 {
+                    value = value + drag_x;
+                }
+            }
+
+            value * args.dt * FPS_FACTOR
         };
 
         let new_v_y = if y <= border_top || y >= border_bottom {
             -v_y * args.dt * FPS_FACTOR
         } else {
-            v_y * args.dt * FPS_FACTOR
+            let mut value = v_y;
+            if inside_ball {
+                if value > 1.0 {
+                    value = value - drag_y
+                } else if value < -1.0 {
+                    value = value + drag_y
+                }
+            }
+
+            value * args.dt * FPS_FACTOR
         };
 
+        // Update velocity
         self.velocity = [new_v_x, new_v_y];
+
+        // Update color
+        let red_factor = ((new_v_x.abs()) / (1.0 + new_v_x.abs() + new_v_y.abs())) as f32
+            + ((new_v_y.abs()) / (1.0 + new_v_x.abs() + new_v_y.abs())) as f32;
+        let blue_factor = (1.0f32 - red_factor).abs();
+        let green_factor = red_factor / 4.0;
+        self.color = [red_factor, green_factor, blue_factor, 1.0];
 
         // Update rotation
         self.rotation += self.angular_velocity * args.dt;
@@ -77,16 +183,19 @@ impl App {
         let [v_x, v_y] = self.velocity;
 
         if key == Key::A {
-            self.velocity = [v_x - ACCELERATION_FACTOR, v_y];
+            self.velocity = [v_x - 1.0 * ACCELERATION_FACTOR, v_y];
         }
         if key == Key::D {
-            self.velocity = [v_x + ACCELERATION_FACTOR, v_y];
+            self.velocity = [v_x + 1.0 * ACCELERATION_FACTOR, v_y];
         }
         if key == Key::W {
-            self.velocity = [v_x, v_y - ACCELERATION_FACTOR];
+            self.velocity = [v_x, v_y - 1.0 * ACCELERATION_FACTOR];
         }
         if key == Key::S {
-            self.velocity = [v_x, v_y + ACCELERATION_FACTOR];
+            self.velocity = [v_x, v_y + 1.0 * ACCELERATION_FACTOR];
+        }
+        if key == Key::Space {
+            self.velocity = [v_x / ACCELERATION_FACTOR, v_y / ACCELERATION_FACTOR];
         }
     }
 }
--- native/src/main.rs
+++ native/src/main.rs
@@ -10,23 +10,25 @@ extern crate vecmath;
 
 use glutin_window::GlutinWindow as AppWindow;
 
-use opengl_graphics::{GlGraphics, OpenGL};
+use opengl_graphics::*;
 use piston::event_loop::*;
 use piston::input::*;
-use piston::window::{AdvancedWindow, Window, WindowSettings};
+use piston::window::WindowSettings;
 
 mod app;
 mod utils;
 
 use app::App;
 
-pub(crate) const SCREEN_HEIGHT: f64 = 1080f64;
-pub(crate) const SCREEN_WIDTH: f64 = 1920f64;
+pub(crate) const SCREEN_HEIGHT: f64 = 1080.0;
+pub(crate) const SCREEN_WIDTH: f64 = 1920.0;
 pub(crate) const SCREEN_MID_POINT: [f64; 2] = [SCREEN_HEIGHT / 2.0, SCREEN_WIDTH / 2.0];
 pub(crate) const WINDOW_TITLE: &str = "DDD Rust 3D Graphics";
 pub(crate) const FPS_FACTOR: f64 = 120.0;
 pub(crate) const MAX_FPS: u64 = 120;
-pub(crate) const ACCELERATION_FACTOR: f64 = 1.0;
+pub(crate) const ACCELERATION_FACTOR: f64 = 1.1;
+pub(crate) const RADIUS: f64 = 50.0;
+pub(crate) const BALL_RADIUS: f64 = RADIUS * 4.0;
 
 fn main() {
     let opengl = OpenGL::V3_2;
@@ -38,12 +40,17 @@ fn main() {
         .unwrap();
 
     let gl = GlGraphics::new(opengl);
+    let texture_settings = TextureSettings::new().filter(Filter::Nearest);
+    let ref mut glyphs =
+        GlyphCache::new("native/assets/FiraSans-Regular.ttf", (), texture_settings)
+            .expect("Could not load font");
+
     let mut app = App {
         gl,
         angular_velocity: 2.0,
         color: [1.0, 1.0, 1.0, 1.0],
         position: SCREEN_MID_POINT,
-        radius: 50.0,
+        radius: RADIUS,
         rotation: 0.0,
         velocity: [1.0, 1.0],
     };
@@ -56,13 +63,14 @@ fn main() {
         ups_reset: 2,       // Default 2
         swap_buffers: true, // Default true
     });
+
     while let Some(e) = events.next(&mut window) {
         if let Some(Button::Keyboard(key)) = e.press_args() {
             app.update_on_input(key);
         }
 
         if let Some(args) = e.render_args() {
-            app.render(&args);
+            app.render::<GlGraphics, GlyphCache>(&args, glyphs);
         }
 
         if let Some(args) = e.update_args() {
