c047e9006163678808dd574a3bd281d5d806cec8
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-12-07 21:10:57 +0100

    Make it 3D - Add imported Blender .obj file - Update demo image

--- native/src/app.rs
+++ native/src/app.rs
@@ -1,225 +1,225 @@
-use std::{
-    collections::VecDeque,
-    time::{Duration, Instant},
-};
+// use std::{
+//     collections::VecDeque,
+//     time::{Duration, Instant},
+// };
 
-use graphics::{CharacterCache, Graphics};
-use opengl_graphics::GlGraphics;
-use piston::{Key, RenderArgs, UpdateArgs};
+// use graphics::{CharacterCache, Graphics};
+// use opengl_graphics::GlGraphics;
+// use piston::{Key, RenderArgs, UpdateArgs};
 
-use crate::{ACCELERATION_FACTOR, BALL_RADIUS, FPS_FACTOR, RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH};
+// use crate::{ACCELERATION_FACTOR, BALL_RADIUS, FPS_FACTOR, RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH};
 
-pub struct App {
-    pub(crate) gl: GlGraphics, // OpenGL drawing backend.
-    pub(crate) velocity: [f64; 2],
-    pub(crate) position: [f64; 2],
-    pub(crate) rotation: f64,
-    pub(crate) angular_velocity: f64,
-    pub(crate) radius: f64,
-    pub(crate) color: [f32; 4],
-    pub(crate) fps: usize,
-    pub(crate) last_second_frames: VecDeque<Instant>,
-}
+// pub struct App {
+//     pub(crate) gl: GlGraphics, // OpenGL drawing backend.
+//     pub(crate) velocity: [f64; 2],
+//     pub(crate) position: [f64; 2],
+//     pub(crate) rotation: f64,
+//     pub(crate) angular_velocity: f64,
+//     pub(crate) radius: f64,
+//     pub(crate) color: [f32; 4],
+//     pub(crate) fps: usize,
+//     pub(crate) last_second_frames: VecDeque<Instant>,
+// }
 
-impl App {
-    pub(crate) fn render<G: Graphics, C>(&mut self, args: &RenderArgs, glyphs: &mut C)
-    where
-        C: CharacterCache<Texture = opengl_graphics::Texture>,
-    {
-        use graphics::*;
+// impl App {
+//     pub(crate) fn render<G: Graphics, C>(&mut self, args: &RenderArgs, glyphs: &mut C)
+//     where
+//         C: CharacterCache<Texture = opengl_graphics::Texture>,
+//     {
+//         use graphics::*;
 
-        self.fps = self.tick();
+//         self.fps = self.tick();
 
-        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
-        const BALL_COLOR: [f32; 4] = [0.0, 0.0, 0.55, 0.3];
+//         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+//         const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+//         const BALL_COLOR: [f32; 4] = [0.0, 0.0, 0.55, 0.3];
 
-        let radius = self.radius;
-        let color = self.color;
-        let square = graphics::rectangle::square(0.0, 0.0, radius * 2.0);
-        let ball = graphics::ellipse::circle(0.0, 0.0, BALL_RADIUS);
-        let rotation = self.rotation;
-        let (_x_screen_mid, _y_screen_mid) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
-        let [x, y] = self.position;
-        let speed = (self.velocity[0].powi(2) + self.velocity[1].powi(2)).sqrt();
-        let fps = self.fps;
+//         let radius = self.radius;
+//         let color = self.color;
+//         let square = graphics::rectangle::square(0.0, 0.0, radius * 2.0);
+//         let ball = graphics::ellipse::circle(0.0, 0.0, BALL_RADIUS);
+//         let rotation = self.rotation;
+//         let (_x_screen_mid, _y_screen_mid) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+//         let [x, y] = self.position;
+//         let speed = (self.velocity[0].powi(2) + self.velocity[1].powi(2)).sqrt();
+//         let fps = self.fps;
 
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(BLACK, gl);
+//         self.gl.draw(args.viewport(), |c, gl| {
+//             // Clear the screen.
+//             clear(BLACK, gl);
 
-            // println!("Square position: {} x {}", x, y);
+//             // println!("Square position: {} x {}", x, y);
 
-            let square_transform = c
-                .transform
-                .trans(x, y)
-                .rot_rad(rotation)
-                .trans(-radius, -radius);
-            let ball_transform = c.transform.trans(_x_screen_mid, _y_screen_mid);
-            let text_title_transform = c.transform.trans(100.0, 100.0);
-            let text_ball_transform = c.transform.trans(
-                args.window_size[0] / 2.0 - BALL_RADIUS / 2.0,
-                args.window_size[1] / 2.0,
-            );
-            let text_keys_transform = c.transform.trans(100.0, 150.0);
-            let text_speed_transform = c.transform.trans(100.0, 200.0);
-            let text_position_transform = c.transform.trans(100.0, 250.0);
-            let text_fps_transform = c.transform.trans(100.0, 300.0);
+//             let square_transform = c
+//                 .transform
+//                 .trans(x, y)
+//                 .rot_rad(rotation)
+//                 .trans(-radius, -radius);
+//             let ball_transform = c.transform.trans(_x_screen_mid, _y_screen_mid);
+//             let text_title_transform = c.transform.trans(100.0, 100.0);
+//             let text_ball_transform = c.transform.trans(
+//                 args.window_size[0] / 2.0 - BALL_RADIUS / 2.0,
+//                 args.window_size[1] / 2.0,
+//             );
+//             let text_keys_transform = c.transform.trans(100.0, 150.0);
+//             let text_speed_transform = c.transform.trans(100.0, 200.0);
+//             let text_position_transform = c.transform.trans(100.0, 250.0);
+//             let text_fps_transform = c.transform.trans(100.0, 300.0);
 
-            let text_title = "Demo";
-            let text_ball = "Drag applied here!";
-            let text_keys = "Keybindings: Acceleration - WASD, Break - Spacebar, ESC - Quit";
-            let text_position = format!("Position x: {} , y: {}", x.round(), y.round());
-            let text_speed = format!("Speed: {}", speed.round());
-            let text_fps = format!("FPS: {}", fps);
+//             let text_title = "Demo";
+//             let text_ball = "Drag applied here!";
+//             let text_keys = "Keybindings: Acceleration - WASD, Break - Spacebar, ESC - Quit";
+//             let text_position = format!("Position x: {} , y: {}", x.round(), y.round());
+//             let text_speed = format!("Speed: {}", speed.round());
+//             let text_fps = format!("FPS: {}", fps);
 
-            rectangle(color, square, square_transform, gl);
-            ellipse(BALL_COLOR, ball, ball_transform, gl);
+//             rectangle(color, square, square_transform, gl);
+//             ellipse(BALL_COLOR, ball, ball_transform, gl);
 
-            graphics::text::Text::new_color(WHITE, 32)
-                .draw(text_title, glyphs, &c.draw_state, text_title_transform, gl)
-                .ok();
-            graphics::text::Text::new_color(WHITE, 24)
-                .draw(text_ball, glyphs, &c.draw_state, text_ball_transform, gl)
-                .ok();
-            graphics::text::Text::new_color(WHITE, 24)
-                .draw(text_keys, glyphs, &c.draw_state, text_keys_transform, gl)
-                .ok();
-            graphics::text::Text::new_color(WHITE, 24)
-                .draw(
-                    text_position.as_str(),
-                    glyphs,
-                    &c.draw_state,
-                    text_position_transform,
-                    gl,
-                )
-                .ok();
-            graphics::text::Text::new_color(WHITE, 24)
-                .draw(
-                    text_speed.as_str(),
-                    glyphs,
-                    &c.draw_state,
-                    text_speed_transform,
-                    gl,
-                )
-                .ok();
-            graphics::text::Text::new_color(WHITE, 24)
-                .draw(
-                    text_fps.as_str(),
-                    glyphs,
-                    &c.draw_state,
-                    text_fps_transform,
-                    gl,
-                )
-                .ok();
-        });
-    }
+//             graphics::text::Text::new_color(WHITE, 32)
+//                 .draw(text_title, glyphs, &c.draw_state, text_title_transform, gl)
+//                 .ok();
+//             graphics::text::Text::new_color(WHITE, 24)
+//                 .draw(text_ball, glyphs, &c.draw_state, text_ball_transform, gl)
+//                 .ok();
+//             graphics::text::Text::new_color(WHITE, 24)
+//                 .draw(text_keys, glyphs, &c.draw_state, text_keys_transform, gl)
+//                 .ok();
+//             graphics::text::Text::new_color(WHITE, 24)
+//                 .draw(
+//                     text_position.as_str(),
+//                     glyphs,
+//                     &c.draw_state,
+//                     text_position_transform,
+//                     gl,
+//                 )
+//                 .ok();
+//             graphics::text::Text::new_color(WHITE, 24)
+//                 .draw(
+//                     text_speed.as_str(),
+//                     glyphs,
+//                     &c.draw_state,
+//                     text_speed_transform,
+//                     gl,
+//                 )
+//                 .ok();
+//             graphics::text::Text::new_color(WHITE, 24)
+//                 .draw(
+//                     text_fps.as_str(),
+//                     glyphs,
+//                     &c.draw_state,
+//                     text_fps_transform,
+//                     gl,
+//                 )
+//                 .ok();
+//         });
+//     }
 
-    pub(crate) fn update(&mut self, args: &UpdateArgs) {
-        let [x, y] = self.position;
-        let inside_ball = is_inside_ball([x, y]);
+//     pub(crate) fn update(&mut self, args: &UpdateArgs) {
+//         let [x, y] = self.position;
+//         let inside_ball = is_inside_ball([x, y]);
 
-        let [v_x, v_y] = self.velocity;
-        let speed = (v_x.powi(2) + v_y.powi(2)).sqrt();
+//         let [v_x, v_y] = self.velocity;
+//         let speed = (v_x.powi(2) + v_y.powi(2)).sqrt();
 
-        // Update scale
-        self.radius = RADIUS + (if speed >= 50.0 { 50.0 } else { speed });
-        let area = 2.0 * self.radius;
+//         // Update scale
+//         self.radius = RADIUS + (if speed >= 50.0 { 50.0 } else { speed });
+//         let area = 2.0 * self.radius;
 
-        // Detect collision with walls
-        let border_left = self.radius;
-        let border_right = SCREEN_WIDTH - self.radius;
-        let border_bottom = SCREEN_HEIGHT - self.radius;
-        let border_top = self.radius;
-        fn is_inside_ball(pos: [f64; 2]) -> bool {
-            let vector_to_center = [980.0 - pos[0], 540.0 - pos[1]];
-            let distance_from_center =
-                ((vector_to_center[0]).powi(2) + (vector_to_center[1]).powi(2)).sqrt();
+//         // Detect collision with walls
+//         let border_left = self.radius;
+//         let border_right = SCREEN_WIDTH - self.radius;
+//         let border_bottom = SCREEN_HEIGHT - self.radius;
+//         let border_top = self.radius;
+//         fn is_inside_ball(pos: [f64; 2]) -> bool {
+//             let vector_to_center = [980.0 - pos[0], 540.0 - pos[1]];
+//             let distance_from_center =
+//                 ((vector_to_center[0]).powi(2) + (vector_to_center[1]).powi(2)).sqrt();
 
-            distance_from_center <= BALL_RADIUS
-        }
-        let drag_x = 0.5 * (1.0 / v_x.abs()) * v_x.powi(2) * area * args.dt;
-        let drag_y = 0.5 * (1.0 / v_y.abs()) * v_y.powi(2) * area * args.dt;
+//             distance_from_center <= BALL_RADIUS
+//         }
+//         let drag_x = 0.5 * (1.0 / v_x.abs()) * v_x.powi(2) * area * args.dt;
+//         let drag_y = 0.5 * (1.0 / v_y.abs()) * v_y.powi(2) * area * args.dt;
 
-        let new_v_x = if x <= border_left || x >= border_right {
-            -v_x * args.dt * FPS_FACTOR
-        } else {
-            let mut value = v_x;
-            if inside_ball {
-                if value > 1.0 {
-                    value = value - drag_x;
-                } else if value < -1.0 {
-                    value = value + drag_x;
-                }
-            }
+//         let new_v_x = if x <= border_left || x >= border_right {
+//             -v_x * args.dt * FPS_FACTOR
+//         } else {
+//             let mut value = v_x;
+//             if inside_ball {
+//                 if value > 1.0 {
+//                     value = value - drag_x;
+//                 } else if value < -1.0 {
+//                     value = value + drag_x;
+//                 }
+//             }
 
-            value * args.dt * FPS_FACTOR
-        };
+//             value * args.dt * FPS_FACTOR
+//         };
 
-        let new_v_y = if y <= border_top || y >= border_bottom {
-            -v_y * args.dt * FPS_FACTOR
-        } else {
-            let mut value = v_y;
-            if inside_ball {
-                if value > 1.0 {
-                    value = value - drag_y
-                } else if value < -1.0 {
-                    value = value + drag_y
-                }
-            }
+//         let new_v_y = if y <= border_top || y >= border_bottom {
+//             -v_y * args.dt * FPS_FACTOR
+//         } else {
+//             let mut value = v_y;
+//             if inside_ball {
+//                 if value > 1.0 {
+//                     value = value - drag_y
+//                 } else if value < -1.0 {
+//                     value = value + drag_y
+//                 }
+//             }
 
-            value * args.dt * FPS_FACTOR
-        };
+//             value * args.dt * FPS_FACTOR
+//         };
 
-        // Update velocity
-        self.velocity = [new_v_x, new_v_y];
+//         // Update velocity
+//         self.velocity = [new_v_x, new_v_y];
 
-        // Update color
-        let red_factor = ((new_v_x.abs()) / (1.0 + new_v_x.abs() + new_v_y.abs())) as f32
-            + ((new_v_y.abs()) / (1.0 + new_v_x.abs() + new_v_y.abs())) as f32;
-        let blue_factor = (1.0f32 - red_factor).abs();
-        let green_factor = red_factor / 4.0;
-        self.color = [red_factor, green_factor, blue_factor, 1.0];
+//         // Update color
+//         let red_factor = ((new_v_x.abs()) / (1.0 + new_v_x.abs() + new_v_y.abs())) as f32
+//             + ((new_v_y.abs()) / (1.0 + new_v_x.abs() + new_v_y.abs())) as f32;
+//         let blue_factor = (1.0f32 - red_factor).abs();
+//         let green_factor = red_factor / 4.0;
+//         self.color = [red_factor, green_factor, blue_factor, 1.0];
 
-        // Update rotation
-        self.rotation += self.angular_velocity * args.dt;
+//         // Update rotation
+//         self.rotation += self.angular_velocity * args.dt;
 
-        // Update position
-        self.position = [x + self.velocity[0], y + self.velocity[1]];
-    }
+//         // Update position
+//         self.position = [x + self.velocity[0], y + self.velocity[1]];
+//     }
 
-    pub(crate) fn update_on_input(&mut self, key: Key) {
-        let [v_x, v_y] = self.velocity;
+//     pub(crate) fn update_on_input(&mut self, key: Key) {
+//         let [v_x, v_y] = self.velocity;
 
-        if key == Key::A {
-            self.velocity = [v_x - 1.0 * ACCELERATION_FACTOR, v_y];
-        }
-        if key == Key::D {
-            self.velocity = [v_x + 1.0 * ACCELERATION_FACTOR, v_y];
-        }
-        if key == Key::W {
-            self.velocity = [v_x, v_y - 1.0 * ACCELERATION_FACTOR];
-        }
-        if key == Key::S {
-            self.velocity = [v_x, v_y + 1.0 * ACCELERATION_FACTOR];
-        }
-        if key == Key::Space {
-            self.velocity = [v_x / ACCELERATION_FACTOR, v_y / ACCELERATION_FACTOR];
-        }
-    }
-    pub fn tick(&mut self) -> usize {
-        let now = Instant::now();
-        let a_second_ago = now - Duration::from_secs(1);
+//         if key == Key::A {
+//             self.velocity = [v_x - 1.0 * ACCELERATION_FACTOR, v_y];
+//         }
+//         if key == Key::D {
+//             self.velocity = [v_x + 1.0 * ACCELERATION_FACTOR, v_y];
+//         }
+//         if key == Key::W {
+//             self.velocity = [v_x, v_y - 1.0 * ACCELERATION_FACTOR];
+//         }
+//         if key == Key::S {
+//             self.velocity = [v_x, v_y + 1.0 * ACCELERATION_FACTOR];
+//         }
+//         if key == Key::Space {
+//             self.velocity = [v_x / ACCELERATION_FACTOR, v_y / ACCELERATION_FACTOR];
+//         }
+//     }
+//     pub fn tick(&mut self) -> usize {
+//         let now = Instant::now();
+//         let a_second_ago = now - Duration::from_secs(1);
 
-        while self
-            .last_second_frames
-            .front()
-            .map_or(false, |t| *t < a_second_ago)
-        {
-            self.last_second_frames.pop_front();
-        }
+//         while self
+//             .last_second_frames
+//             .front()
+//             .map_or(false, |t| *t < a_second_ago)
+//         {
+//             self.last_second_frames.pop_front();
+//         }
 
-        self.last_second_frames.push_back(now);
-        self.last_second_frames.len()
-    }
-}
+//         self.last_second_frames.push_back(now);
+//         self.last_second_frames.len()
+//     }
+// }
--- native/src/main.rs
+++ native/src/main.rs
@@ -1,81 +1,207 @@
+extern crate camera_controllers;
 extern crate glutin_window;
 extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 extern crate piston_window;
 extern crate vecmath;
+#[macro_use]
+extern crate gfx;
+extern crate shader_version;
 
-use std::collections::VecDeque;
-
-use glutin_window::GlutinWindow as AppWindow;
-
+use camera_controllers::{
+    model_view_projection, CameraPerspective, FirstPerson, FirstPersonSettings,
+};
+use gfx::traits::*;
+use graphics::color::WHITE;
+use obj::*;
 use opengl_graphics::*;
-use piston::event_loop::*;
 use piston::input::*;
-use piston::window::WindowSettings;
-
-mod app;
-mod utils;
-
-use app::App;
+use piston_window::*;
+use shader_version::glsl::GLSL;
+use shader_version::Shaders;
+use std::fs::File;
+use std::io::BufReader;
 
 pub(crate) const SCREEN_HEIGHT: f64 = 1080.0;
 pub(crate) const SCREEN_WIDTH: f64 = 1920.0;
-pub(crate) const SCREEN_MID_POINT: [f64; 2] = [SCREEN_HEIGHT / 2.0, SCREEN_WIDTH / 2.0];
 pub(crate) const WINDOW_TITLE: &str = "DDD Rust 3D Graphics";
-pub(crate) const FPS_FACTOR: f64 = 240.0;
-pub(crate) const MAX_FPS: u64 = 120;
-pub(crate) const ACCELERATION_FACTOR: f64 = 1.1;
-pub(crate) const RADIUS: f64 = 50.0;
-pub(crate) const BALL_RADIUS: f64 = RADIUS * 4.0;
+pub(crate) const INITIAL_CAMERA_POSITION: [f32; 3] = [2.0, 2.0, 5.0];
+
+gfx_vertex_struct!(Vertex {
+    a_pos: [f32; 4] = "a_pos",
+    a_tex_coord: [f32; 2] = "a_tex_coord",
+});
+
+impl Vertex {
+    fn new(pos: [f32; 3], tc: [f32; 2]) -> Vertex {
+        Vertex {
+            a_pos: [pos[0], pos[1], pos[2], 1.0],
+            a_tex_coord: tc,
+        }
+    }
+}
+
+gfx_pipeline!( pipe {
+    vbuf: gfx::VertexBuffer<Vertex> = (),
+    u_model_view_proj: gfx::Global<[[f32; 4]; 4]> = "u_model_view_proj",
+    t_color: gfx::TextureSampler<[f32; 4]> = "t_color",
+    out_color: gfx::RenderTarget<::gfx::format::Srgba8> = "o_Color",
+    out_depth: gfx::DepthTarget<::gfx::format::DepthStencil> =
+        gfx::preset::depth::LESS_EQUAL_WRITE,
+});
+
+fn get_glowing_cube_obj() -> Obj {
+    let file = File::open("native/assets/glowing_cube.obj").unwrap();
+    let input = BufReader::new(file);
+    let model: Obj = load_obj(input).unwrap();
+
+    model
+}
 
 fn main() {
     let opengl = OpenGL::V3_2;
 
-    let mut window: AppWindow = WindowSettings::new(WINDOW_TITLE, [SCREEN_WIDTH, SCREEN_HEIGHT])
+    let mut window: PistonWindow = WindowSettings::new(WINDOW_TITLE, [SCREEN_WIDTH, SCREEN_HEIGHT])
         .graphics_api(opengl)
+        .samples(4)
         .exit_on_esc(true)
         .build()
         .unwrap();
+    window.set_capture_cursor(true);
 
-    let gl = GlGraphics::new(opengl);
-    let texture_settings = TextureSettings::new().filter(Filter::Nearest);
-    let ref mut glyphs =
-        GlyphCache::new("native/assets/FiraSans-Regular.ttf", (), texture_settings)
-            .expect("Could not load font");
+    // Load model data from .obj file, exported from Blender
+    let cube_obj = get_glowing_cube_obj();
+    let vertex_data: Vec<Vertex> = cube_obj
+        .vertices
+        .into_iter()
+        .map(|vert| Vertex::new(vert.position, [vert.normal[0], vert.normal[1]]))
+        .collect();
+    let index_data = cube_obj.indices.as_slice();
 
-    let mut app = App {
-        gl,
-        angular_velocity: 2.0,
-        color: [1.0, 1.0, 1.0, 1.0],
-        position: SCREEN_MID_POINT,
-        radius: RADIUS,
-        rotation: 0.0,
-        velocity: [1.0, 1.0],
-        fps: 0,
-        last_second_frames: VecDeque::with_capacity(128),
+    let ref mut factory = window.factory.clone();
+
+    let (vbuf, slice) = factory.create_vertex_buffer_with_slice(&vertex_data, index_data);
+
+    let texels = [
+        [0xff, 0xff, 0xff, 0x00],
+        [0xff, 0x00, 0xff, 0x00],
+        [0x00, 0xff, 0x00, 0xff],
+        [0xff, 0x00, 0xff, 0x00],
+    ];
+    let (_, texture_view) = factory
+        .create_texture_immutable::<gfx::format::Rgba8>(
+            gfx::texture::Kind::D2(2, 2, gfx::texture::AaMode::Single),
+            gfx::texture::Mipmap::Provided,
+            &[&texels],
+        )
+        .unwrap();
+
+    let sinfo = gfx::texture::SamplerInfo::new(
+        gfx::texture::FilterMethod::Trilinear,
+        gfx::texture::WrapMode::Clamp,
+    );
+
+    let glsl = opengl.to_glsl();
+    let pipeline_state = factory
+        .create_pipeline_simple(
+            Shaders::new()
+                .set(GLSL::V1_20, include_str!("../assets/cube_120.glslv"))
+                .set(GLSL::V1_50, include_str!("../assets/cube_150.glslv"))
+                .get(glsl)
+                .unwrap()
+                .as_bytes(),
+            Shaders::new()
+                .set(GLSL::V1_20, include_str!("../assets/cube_120.glslf"))
+                .set(GLSL::V1_50, include_str!("../assets/cube_150.glslf"))
+                .get(glsl)
+                .unwrap()
+                .as_bytes(),
+            pipe::new(),
+        )
+        .unwrap();
+
+    let get_projection = |w: &PistonWindow| {
+        let draw_size = w.window.draw_size();
+        CameraPerspective {
+            fov: 70.0,
+            near_clip: 0.1,
+            far_clip: 1000.0,
+            aspect_ratio: (draw_size.width as f32) / (draw_size.height as f32),
+        }
+        .projection()
     };
 
-    let mut events = Events::new(EventSettings {
-        bench_mode: false,          // Default false
-        lazy: false,                // Default false
-        max_fps: MAX_FPS,           // Default 60
-        ups: MAX_FPS * 2u64,        // Default 120
-        ups_reset: MAX_FPS / 30u64, // Default 2
-        swap_buffers: true,         // Default true
-    });
+    let model = vecmath::mat4_id();
+    let mut projection = get_projection(&window);
+    let mut first_person_camera = FirstPerson::new(
+        INITIAL_CAMERA_POSITION,
+        FirstPersonSettings::keyboard_wasd(),
+    );
 
-    while let Some(e) = events.next(&mut window) {
-        if let Some(Button::Keyboard(key)) = e.press_args() {
-            app.update_on_input(key);
-        }
+    let mut data = pipe::Data {
+        vbuf: vbuf.clone(),
+        u_model_view_proj: [[0.0; 4]; 4],
+        t_color: (texture_view, factory.create_sampler(sinfo)),
+        out_color: window.output_color.clone(),
+        out_depth: window.output_stencil.clone(),
+    };
 
-        if let Some(args) = e.render_args() {
-            app.render::<GlGraphics, GlyphCache>(&args, glyphs);
-        }
+    // let texture_settings = TextureSettings::new().filter(Filter::Nearest);
 
-        if let Some(args) = e.update_args() {
-            app.update(&args);
+    // let ref mut glyphs =
+    //     GlyphCache::new("native/assets/FiraSans-Regular.ttf", (), texture_settings)
+    //         .expect("Could not load font");
+
+    while let Some(e) = window.next() {
+        first_person_camera.event(&e);
+
+        window.draw_3d(&e, |window| {
+            let args = e.render_args().unwrap();
+
+            window
+                .encoder
+                .clear(&window.output_color, [0.3, 0.3, 0.3, 1.0]);
+            window.encoder.clear_depth(&window.output_stencil, 1.0);
+
+            data.u_model_view_proj = model_view_projection(
+                model,
+                first_person_camera.camera(args.ext_dt).orthogonal(),
+                projection,
+            );
+            window.encoder.draw(&slice, &pipeline_state, &data);
+        });
+
+        // window.draw_2d(&e, |c, gl_graphics: &mut Graphics<Texture = <C as CharacterCache>::Texture>, device| {
+        //     let text_title_transform = c.transform.trans(100.0, 100.0);
+        //     let text_keys_transform = c.transform.trans(100.0, 150.0);
+        //     let text_title = "Demo";
+        //     let text_keys = "Keybindings: Acceleration - WASD, Break - Spacebar, ESC - Quit";
+
+        //     graphics::text::Text::new_color(WHITE, 32)
+        //         .draw(
+        //             text_title,
+        //             glyphs,
+        //             &c.draw_state,
+        //             text_title_transform,
+        //             gl_graphics,
+        //         )
+        //         .ok();
+        //     graphics::text::Text::new_color(WHITE, 24)
+        //         .draw(
+        //             text_keys,
+        //             glyphs,
+        //             &c.draw_state,
+        //             text_keys_transform,
+        //             gl_graphics,
+        //         )
+        //         .ok();
+        // });
+
+        if let Some(_) = e.resize_args() {
+            projection = get_projection(&window);
+            data.out_color = window.output_color.clone();
+            data.out_depth = window.output_stencil.clone();
         }
     }
 }
